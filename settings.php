9a0133728a45af71d7c33068cd51bc1600b02e28
Luca <Luck.luca95@gmail.com>
2020-09-16 14:57:12 +0200

    Update settings.php

--- settings.php
+++ settings.php
@@ -3,5 +3,5 @@
 $bitlytoken = 'abcde1234example'; //inserisci qui dentro il token bitly [CAMBPO OBBLIGATORIO]
 $api = '123456789:AAexample'; //inserisci l'api token del tuo bot telegram [CAMBPO OBBLIGATORIO]
 $chats = [12345678, -1000234567]; //inserisci qui dentro i chatID che possono usare il bot se vuoi.
-$ref = 'gorre0307-21'; //inserisci qui dentro il tuo tag amazon.
+$ref = 'luckyflo95-21'; //inserisci qui dentro il tuo tag amazon.
 $amzn = false; //Metti true per referrare anche i link amzn.to (sconsigliato). Consigliato lasciare false.
